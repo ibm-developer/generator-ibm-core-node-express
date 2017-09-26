accdc769c34b3653e92e16fab70a267f5c025687
Gibson Fahnestock <gibfahn@gmail.com>
2017-09-26 20:17:49 +0100

    chore: tabs to spaces, remove trailing whitespace

--- app/index.js
+++ app/index.js
@@ -29,6 +29,10 @@ module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
 
+    if ( typeof opts.bluemix.quiet == "undefined" || ! opts.bluemix.quiet ) {
+      logger.info("Package info ::", Bundle.name, Bundle.version);
+    }
+
     //  bluemix option for YaaS integration
     this.argument(OPTION_BLUEMIX, {
       desc: 'Option for deploying with Bluemix. Stringified JSON.',
@@ -50,16 +54,16 @@ module.exports = class extends Generator {
     this.skipPrompt = true;
     let bluemix_ok= this._sanitizeOption(this.options, OPTION_BLUEMIX);
     let spec_ok= this._sanitizeOption(this.options, OPTION_SPEC);
-    if ( ! (bluemix_ok || spec_ok )) throw ("Must specify either bluemix or spec parameter");  
-            
-    if ( typeof this.options.bluemix.quiet == "undefined" || ! this.options.bluemix.quiet ) { 
+    if ( ! (bluemix_ok || spec_ok )) throw ("Must specify either bluemix or spec parameter");
+
+    if ( typeof this.options.bluemix.quiet == "undefined" || ! this.options.bluemix.quiet ) {
       logger.info("Package info ::", Bundle.name, Bundle.version);
-    } 
-    
+    }
+
     let appName = this.options.bluemix.name || this.options.spec.appname;
     this.options.sanitizedAppName = this._sanitizeAppName(appName);
-    this.options.genSwagger= false; 
-    this.options.openApiFileType= "yaml"; // default 
+    this.options.genSwagger= false;
+    this.options.openApiFileType= "yaml"; // default
 
     this.options.parsedSwagger = undefined;
     let formatters = {
@@ -75,8 +79,8 @@ module.exports = class extends Generator {
       .then(response => {
         this.options.loadedApi = response.loaded;
         this.options.parsedSwagger = response.parsed;
-        this.options.openApiFileType= response.type; 
-        this.options.genSwagger= true; 
+        this.options.openApiFileType= response.type;
+        this.options.genSwagger= true;
       })
       .catch(err => {
         err.message = 'failed to parse document from bluemix.openApiServers ' + err.message;
@@ -84,9 +88,9 @@ module.exports = class extends Generator {
       })
     }
 
-    // micro service always gets swagger ui and no public 
+    // micro service always gets swagger ui and no public
     if(this.options.spec && this.options.spec.applicationType === 'MS') {
-      this.options.genSwagger= true; 
+      this.options.genSwagger= true;
     }
 
   }
@@ -116,21 +120,21 @@ module.exports = class extends Generator {
     this.fs.copyTpl(this.templatePath('package.json'), this.destinationPath('package.json'), this.options);
     this.fs.copyTpl(this.templatePath('README.md'), this.destinationPath('README.md'), this.options);
 
-    // if project will have swagger doc, ensure swagger ui and api route 
+    // if project will have swagger doc, ensure swagger ui and api route
     if ( this.options.genSwagger ) {
       this.fs.copy(this.templatePath('public/swagger-ui'), this.destinationPath('public/swagger-ui'));
-      // if open api doc provided, write it else write default 
+      // if open api doc provided, write it else write default
 
       if ( this.options.loadedApi ) {
         let yaml= this.options.bluemix.openApiServers[0].spec;
         //this.fs.writeJSON('public/swagger.'+this.options.openApiFileType, this.options.loadedApi);
-        this.fs.write('public/swagger.'+this.options.openApiFileType, yaml);      
-      } 
+        this.fs.write('public/swagger.'+this.options.openApiFileType, yaml);
+      }
       else {
         this.fs.copyTpl(this.templatePath('public/swagger.yaml'), this.destinationPath('public/swagger.yaml'), this.options);
       }
     }
-    else { 
+    else {
       this.fs.delete(this.destinationPath('server/routers/swagger.js'));
     }
 
@@ -138,11 +142,11 @@ module.exports = class extends Generator {
     if( this.options.genSwagger ) {
       this.fs.delete(this.destinationPath('server/routers/public.js'));
     }
-    else { 
+    else {
       this.fs.copy(this.templatePath('public/index.html'), this.destinationPath('public/index.html'));
     }
 
-    // blank project is stripped down to bare minimum 
+    // blank project is stripped down to bare minimum
     if(this.options.spec && this.options.spec.applicationType === 'BLANK') {
       this.fs.delete(this.destinationPath('server/routers/health.js'));
     }
@@ -153,12 +157,12 @@ module.exports = class extends Generator {
     return name.toLowerCase().replace(REGEX_LEADING_ALPHA, '').replace(REGEX_ALPHA_NUM, '');
   }
 
-	// return true if 'sanitized', false if missing, exception if bad data 
+  // return true if 'sanitized', false if missing, exception if bad data
   _sanitizeOption(options, name) {
     let optionValue = options[name];
     if (!optionValue) {
       logger.error("Missing", name, "parameter");
-      return false; 
+      return false;
     }
 
     if (typeof optionValue === "string" && optionValue.indexOf("file:") === 0) {
@@ -166,16 +170,16 @@ module.exports = class extends Generator {
       let filePath = this.destinationPath("./" + fileName);
       logger.info("Reading", name, "parameter from local file", filePath);
       this.options[name] = this.fs.readJSON(filePath);
-      return true; 
+      return true;
     }
 
     try {
       this.options[name] = typeof(this.options[name]) === "string" ?
       JSON.parse(this.options[name]) : this.options[name];
-      return true; 
+      return true;
     } catch (e) {
       logger.error(e);
       throw name + " parameter is expected to be a valid stringified JSON object";
-    }   
+    }
   }
 };
