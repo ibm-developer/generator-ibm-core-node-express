7b0a609b8ffaf86eb847ff6fd9f8fbc489fedf53
Gibson Fahnestock <gibfahn@gmail.com>
2017-09-26 20:17:49 +0100

    chore: tabs to spaces, remove trailing whitespace

--- app/templates/server/server.js
+++ app/templates/server/server.js
@@ -14,12 +14,12 @@ require('./routers/index')(app);
 
 const port = process.env.PORT || localConfig.port;
 app.listen(port, function(){
-	logger.info(`<%= bluemix.name %> listening on http://localhost:${port}/appmetrics-dash`);
-	<% if( !genSwagger ){ %>
-	logger.info(`<%= bluemix.name %> listening on http://localhost:${port}`);
-	<% } %>
-	<% if( genSwagger ){ %>
-	logger.info(`OpenAPI (Swagger) spec is available at http://localhost:${port}/swagger/api`);
-	logger.info(`Swagger UI is available at http://localhost:${port}/explorer`);
-	<% } %>
+  logger.info(`<%= bluemix.name %> listening on http://localhost:${port}/appmetrics-dash`);
+  <% if( !genSwagger ){ %>
+  logger.info(`<%= bluemix.name %> listening on http://localhost:${port}`);
+  <% } %>
+  <% if( genSwagger ){ %>
+  logger.info(`OpenAPI (Swagger) spec is available at http://localhost:${port}/swagger/api`);
+  logger.info(`Swagger UI is available at http://localhost:${port}/explorer`);
+  <% } %>
 });
