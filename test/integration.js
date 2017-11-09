f283f39dc03bd229b57d1d5b3231d3c58470af10
Joy Chang <joychang@us.ibm.com>
2017-11-09 17:47:53 -0600

    feat: Add server-side hot reload

--- test/integration.js
+++ test/integration.js
@@ -67,30 +67,20 @@ describe('core-node-express:app integration test with custom spec', function () 
         },
         "scripts": {
           "start": "node server/server.js",
-          "start:cluster": "sl-run server/server.js",
           "debug": "node --debug server/server.js",
-          "test": "nyc mocha",
-          "build": "npm run build:idt",
-          "idt:build": "node idt.js build",
-          "idt:test": "node idt.js test",
-          "idt:debug": "node idt.js debug",
-          "idt:run": "node idt.js run",
-          "idt:deploy": "node idt.js deploy",
-          "idt:install": "node idt.js install"
+          "test": "nyc mocha"
         },
         "dependencies": {
           "appmetrics-dash": "^3.3.2",
           "appmetrics-prometheus": "^0.0.2",
           "body-parser": "^1.17.2",
           "express": "^4.15.3",
-          "strong-supervisor": "^6.2.0",
           "log4js": "^1.1.1"
         },
         "devDependencies": {
           "chai": "^4.0.0",
           "mocha": "^3.4.2",
-          "nyc": "^10.3.2",
-          "proxyquire": "^1.8.0"
+          "nyc": "^10.3.2"
         }});
     });
   });
@@ -119,6 +109,109 @@ describe('core-node-express:app integration test with custom spec', function () 
   });
 });
 
+describe('core-node-express:app integration test with custom bluemix.fromYo flag', function () {
+  // Express build is slow so we need to set a longer timeout for the test
+  this.timeout(150000);
+
+  before(function () {
+    // Mock the options, set up an output folder and run the generator
+    return helpers.run(path.join( __dirname, '../app'))
+        .withOptions({
+          bluemix: JSON.stringify({ name: PROJECT_NAME, fromYo: true})
+        })
+        .toPromise(); // Get a Promise back when the generator finishes
+  });
+
+  describe('basic file structure test', function () {
+    // Files which we assert are created each time the app generator is run.
+    // Takes an array of files, converted from obj by Object.values().
+    const expected = Object.keys(common.file).map((key) => common.file[key]);
+
+    it('generates the expected application files', function () {
+      assert.file(expected);
+      assert.file('idt.js');
+    });
+  });
+
+  describe(common.file.local, function () {
+    it('contains the default port', function () {
+      assert.jsonFileContent(common.file.local, {port: 3000});
+    });
+  });
+
+  describe(common.file.package_json, function () {
+    it('check package.json', function () {
+      assert.jsonFileContent(common.file.package_json, {
+        "version": "1.0.0",
+        "description": "A generated IBM Cloud application",
+        "private": true,
+        "engines": {
+          "node": "^6.9.0"
+        },
+        "scripts": {
+          "start": "node server/server.js",
+          "test": "nyc mocha",
+          "start:cluster": "sl-run server/server.js",
+          "build": "npm run build:idt",
+          "idt:build": "node idt.js build",
+          "idt:test": "node idt.js test",
+          "idt:debug": "node idt.js debug",
+          "idt:run": "node idt.js run",
+          "idt:deploy": "node idt.js deploy",
+          "idt:install": "node idt.js install"
+        },
+        "dependencies": {
+          "appmetrics-dash": "^3.3.2",
+          "appmetrics-prometheus": "^0.0.2",
+          "body-parser": "^1.17.2",
+          "express": "^4.15.3",
+          "log4js": "^1.1.1",
+          "strong-supervisor": "^6.2.0"
+        },
+        "devDependencies": {
+          "chai": "^4.0.0",
+          "mocha": "^3.4.2",
+          "nyc": "^10.3.2"
+        }});
+    });
+  });
+
+  describe(common.file.README_md, function () {
+    it('contains custom project name', function () {
+      assert.fileContent(common.file.README_md, PROJECT_NAME);
+    });
+
+    it('contains Bluemix badge', function () {
+      assert.fileContent(common.file.README_md,
+          '[![](https://img.shields.io/badge/bluemix-powered-blue.svg)](https://bluemix.net)');
+    });
+  });
+
+  describe(common.file.server_js, () => {
+    it('contains custom app name', () => {
+      assert.fileContent(common.file.server_js, 'logger.info(`ProjectName listening on http://localhost:${port}`);')
+    });
+  });
+
+  describe(common.file.server_js, () => {
+    it('contains appmetrics-dash attach', () => {
+      assert.fileContent(common.file.server_js, "require('appmetrics-dash').attach();")
+    });
+  });
+
+  describe(common.file.server_js, () => {
+    it('contains appmetrics-prometheus attach', () => {
+      assert.fileContent(common.file.server_js, "require('appmetrics-prometheus').attach();")
+    });
+  });
+
+  describe(common.file.gitignore, function () {
+    it('contains node_modules', function () {
+      assert.fileContent(common.file.gitignore, 'node_modules');
+    });
+  });
+});
+
 describe('core-node-express:app integration test with custom bluemix', function () {
   // Express build is slow so we need to set a longer timeout for the test
   this.timeout(150000);
@@ -126,10 +219,10 @@ describe('core-node-express:app integration test with custom bluemix', function 
   before(function () {
     // Mock the options, set up an output folder and run the generator
     return helpers.run(path.join( __dirname, '../app'))
-      .withOptions({
-        bluemix: JSON.stringify({ name: PROJECT_NAME})
-      })
-      .toPromise(); // Get a Promise back when the generator finishes
+        .withOptions({
+          bluemix: JSON.stringify({ name: PROJECT_NAME})
+        })
+        .toPromise(); // Get a Promise back when the generator finishes
   });
 
   describe('basic file structure test', function () {
@@ -171,8 +264,7 @@ describe('core-node-express:app integration test with custom bluemix', function 
         "devDependencies": {
           "chai": "^4.0.0",
           "mocha": "^3.4.2",
-          "nyc": "^10.3.2",
-          "proxyquire": "^1.8.0"
+          "nyc": "^10.3.2"
         }});
     });
   });
@@ -184,7 +276,7 @@ describe('core-node-express:app integration test with custom bluemix', function 
 
     it('contains Bluemix badge', function () {
       assert.fileContent(common.file.README_md,
-        '[![](https://img.shields.io/badge/bluemix-powered-blue.svg)](https://bluemix.net)');
+          '[![](https://img.shields.io/badge/bluemix-powered-blue.svg)](https://bluemix.net)');
     });
   });
 
@@ -266,8 +358,7 @@ describe('core-node-express:app integration test with custom bluemix and spec', 
         "devDependencies": {
           "chai": "^4.0.0",
           "mocha": "^3.4.2",
-          "nyc": "^10.3.2",
-          "proxyquire": "^1.8.0"
+          "nyc": "^10.3.2"
         }});
     });
   });
