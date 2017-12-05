1fc8eb8f2af7c5d7d28d290824ac2a2fbb5293f3
jkingoliver <king@us.ibm.com>
2017-12-05 15:01:48 -0600

    feat(node): add updated success page for running app and add routes for 404 and 500 errors

--- app/templates/server/server.js
+++ app/templates/server/server.js
@@ -5,6 +5,7 @@ const appName = require('./../package').name;
 const express = require('express');
 const log4js = require('log4js');
 const localConfig = require('./config/local.json');
+const path = require('path');
 
 const logger = log4js.getLogger(appName);
 const app = express();
@@ -25,3 +26,11 @@ app.listen(port, function(){
   logger.info(`Swagger UI is available at http://localhost:${port}/explorer`);
   <% } %>
 });
+
+app.use(function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/assets', '404.html'));
+})
+
+app.use(function (err, req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/assets', '500.html'));
+})
